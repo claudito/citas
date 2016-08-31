9b0f9797dcefcfaa8091e4689d10dc5191a326ca
claudito <megabyte1507@gmail.com>
2016-08-31 08:03:55 -0500

    add meta viewport

--- enlances/principal.php
+++ enlances/principal.php
@@ -1,3 +1,6 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
